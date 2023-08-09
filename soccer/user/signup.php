b31dcbb134b1268b965b16d4c201902f0b690c9e
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-08-09 13:59:29 +0500

    updated

---
+++ soccer/user/signup.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up | Admin
+    </title>
+    <?php include('../bootstrap/bootstrap-cdn.html')?>
+</head>
+<body>
+    <section>
+        <div class="container-sm mt-5">
+        <form>
+  <div class="mb-3">
+    
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+        </div>
+    
+    </section>
+</body>
+</html>
