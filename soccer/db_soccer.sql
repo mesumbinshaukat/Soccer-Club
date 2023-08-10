59492d6abcd7d8be2d371b1fac15c878025ddb67
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-08-10 09:51:40 +0500

    committed

--- soccer/db_soccer.sql
+++ soccer/db_soccer.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 09, 2023 at 01:45 PM
+-- Generation Time: Aug 09, 2023 at 11:11 PM
 -- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- PHP Version: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -54,9 +54,23 @@ CREATE TABLE `checkout` (
 
 CREATE TABLE `leagues` (
   `l_id` int(11) NOT NULL,
-  `l_name` int(11) NOT NULL,
-  `l_logo` int(11) NOT NULL
+  `l_name` varchar(500) NOT NULL,
+  `l_logo` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leagues`
+--
+
+INSERT INTO `leagues` (`l_id`, `l_name`, `l_logo`) VALUES
+(5, 'Premier League', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg'),
+(6, 'Premier League', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg'),
+(7, 'Premier League', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg'),
+(8, 'Premier League', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg'),
+(9, 'Premier League', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg'),
+(11, 'FIFA WORLD CUP', 'russia-world-cup (2).jpg0909-0808-232323231691592882'),
+(12, 'FIFA WORLD CUP', 'premier-league-logo-symbol-with-name-design-england-football-european-countries-football-teams-illustration-with-purple-background-free-vector.jpg0909-0808-232323231691592978'),
+(13, 'FIFA WORLD CUP', 'russia-world-cup (2).jpg');
 
 -- --------------------------------------------------------
 
@@ -67,10 +81,8 @@ CREATE TABLE `leagues` (
 CREATE TABLE `leagues_statistics` (
   `id` int(11) NOT NULL,
   `team_id` int(11) NOT NULL,
-  `total_win` int(11) NOT NULL,
-  `total_drawn` int(11) NOT NULL,
-  `total_lost` int(11) NOT NULL,
-  `league_id` int(11) NOT NULL
+  `league_id` int(11) NOT NULL,
+  `match_schedule_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -87,6 +99,13 @@ CREATE TABLE `marchandise` (
   `p_image` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `marchandise`
+--
+
+INSERT INTO `marchandise` (`p_id`, `p_cat`, `p_name`, `p_price`, `p_image`) VALUES
+(2, 2, 'abc', 344, '../product_photos/2023-08-09-23-24654_russia2018_logo.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -97,14 +116,22 @@ CREATE TABLE `match_schedule` (
   `match_id` int(11) NOT NULL,
   `team_1` int(11) NOT NULL,
   `team_2` int(11) NOT NULL,
-  `date` varchar(500) NOT NULL,
+  `date` date DEFAULT NULL,
   `time` varchar(500) NOT NULL,
-  `m_league_id` int(11) NOT NULL,
+  `m_league_id` int(11) DEFAULT NULL,
   `m_status` int(11) DEFAULT NULL,
-  `team_1_goals` int(11) NOT NULL,
-  `team_2_goals` int(11) NOT NULL,
-  `player_of_match` int(11) NOT NULL
+  `team_1_goals` int(11) DEFAULT NULL,
+  `team_2_goals` int(11) DEFAULT NULL,
+  `player_of_match` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `match_schedule`
+--
+
+INSERT INTO `match_schedule` (`match_id`, `team_1`, `team_2`, `date`, `time`, `m_league_id`, `m_status`, `team_1_goals`, `team_2_goals`, `player_of_match`) VALUES
+(1, 2, 3, '2023-08-09', '21:00', 6, 2, 3, 1, 4),
+(2, 4, 2, '2023-08-09', '01:39', 6, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -116,9 +143,17 @@ CREATE TABLE `players` (
   `p_id` int(11) NOT NULL,
   `p_name` varchar(111) NOT NULL,
   `p_pic` varchar(111) NOT NULL,
-  `p_achievement` int(11) NOT NULL,
+  `p_achievement` int(11) DEFAULT NULL,
   `team_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`p_id`, `p_name`, `p_pic`, `p_achievement`, `team_id`) VALUES
+(4, 'Cristiano Ronaldo', '../player_photo/2023-08-09-21-31manchester city.png', NULL, 3),
+(5, 'Lionel Messi', '../player_photo/2023-08-09-21-42654_russia2018_logo.jpg', NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -131,6 +166,15 @@ CREATE TABLE `product_category` (
   `c_name` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `product_category`
+--
+
+INSERT INTO `product_category` (`c_id`, `c_name`) VALUES
+(1, 'Jersey'),
+(2, 'Shoes'),
+(3, 'Player Posters');
+
 -- --------------------------------------------------------
 
 --
@@ -142,11 +186,20 @@ CREATE TABLE `team` (
   `t_name` varchar(500) NOT NULL,
   `t_logo` varchar(500) NOT NULL,
   `t_players_count` int(11) NOT NULL,
-  `league_acheivment_id` int(11) NOT NULL,
-  `total_win` int(11) NOT NULL,
-  `total_drawn` int(11) NOT NULL,
-  `lost` int(11) NOT NULL
+  `league_acheivment_id` int(11) DEFAULT NULL,
+  `total_win` int(11) DEFAULT NULL,
+  `total_drawn` int(11) DEFAULT NULL,
+  `total_lost` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`t_id`, `t_name`, `t_logo`, `t_players_count`, `league_acheivment_id`, `total_win`, `total_drawn`, `total_lost`) VALUES
+(2, 'Arsenal', '../team_logo2023-08-09-18-02Arsenal-Logo.png', 14, NULL, NULL, NULL, NULL),
+(3, 'Liverpool FC', '../team_logo2023-08-09-18-54Liverpool_FC.png', 15, NULL, NULL, NULL, NULL),
+(4, 'Chelsea', '../team_logo2023-08-09-20-46Chelsea_FC.png', 16, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -259,7 +312,7 @@ ALTER TABLE `checkout`
 -- AUTO_INCREMENT for table `leagues`
 --
 ALTER TABLE `leagues`
-  MODIFY `l_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `l_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `leagues_statistics`
@@ -271,31 +324,31 @@ ALTER TABLE `leagues_statistics`
 -- AUTO_INCREMENT for table `marchandise`
 --
 ALTER TABLE `marchandise`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `match_schedule`
 --
 ALTER TABLE `match_schedule`
-  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `product_category`
 --
 ALTER TABLE `product_category`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
