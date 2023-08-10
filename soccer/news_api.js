745aa2189f7dd6a3449225d5cab93a4d4f02b81a
mesumbinshaukat <masumbinshaukat@gmail.com>
2023-08-10 17:07:38 +0500

    comitted

---
+++ soccer/news_api.js
@@ -0,0 +1,24 @@
+
+var url = 'https://newsapi.org/v2/everything?q=football&apiKey=4707ccada6ac4342888507cc4227efd1';
+var req = new Request(url);
+// fetch(req)
+//     .then((response) => 
+//     // var data = response.json();
+//     console.log(response.json()  )
+        
+//         )
+        // fetch(req).then(res => res.json()).then(data => {
+        //     // debugger
+        //     var alldata = data.articles;
+        //     // console.log(data)
+        //     for (let index = 0; index < 5/*alldata.length*/; index++) {
+        //         var element = alldata[index];
+        //         console.log(element)
+        //         console.log(element['title'])
+        //         // console.log(element['url'])
+        //         // console.log(element['content'])
+        //         // console.log(element['description'])
+        //         // console.log(element['author'])
+        //     }
+        // })
+    // .then((data) => console.log(data.list))
---
+++ soccer/news_api.php
@@ -0,0 +1,7 @@
+<script src="news_api.js">
+
+</script>
+
+<h1>
+FOOTBALL NEWS - REST API
+</h1>
