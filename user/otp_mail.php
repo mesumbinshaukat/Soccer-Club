620da968db3f7d6bda92e7f9f216caa3d0aa32af
Mesum Bin Shaukat <masumbinshaukat@gmail.com>
2023-08-18 19:55:54 +0500

    Update otp_mail.php

--- user/otp_mail.php
+++ user/otp_mail.php
@@ -38,8 +38,8 @@ if ($query_run) {
         $mail->isSMTP(); //Send using SMTP
         $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
         $mail->SMTPAuth = true; //Enable SMTP authentication
-        $mail->Username = 'wot.official.786@gmail.com'; //SMTP username
-        $mail->Password = 'rbonmvlzxhgpouvw'; //SMTP password
+        $mail->Username = 'soccer.club.techwiz@gmail.com'; //SMTP username
+        $mail->Password = 'utlettxkuvsxbwkl'; //SMTP password
         $mail->SMTPSecure = 'ssl'; //Enable implicit TLS encryption
         $mail->Port = 465; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
